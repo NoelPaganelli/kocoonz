45c2984022f277d75d31475d3e67881142d06c48
noel paganelli <noel.paganelli@free.fr>
2017-04-06 10:45:53 +0200

    webservice lat et lon et ajout de la map

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var mongoose= require('mongoose');
-var session = require("express-session");
-var fileUpload = require('express-fileupload');
-var bodyParser = require('body-parser')
+var express     = require('express');
+var mongoose    = require('mongoose');
+var session     = require("express-session");
+var fileUpload  = require('express-fileupload');
+var bodyParser  = require('body-parser');
+var request     = require('request');
 
 mongoose.connect('mongodb://localhost/kocoonz' , function(err) {
 
@@ -14,7 +15,9 @@ var homeSchema = mongoose.Schema({
   title:String,
   desc: String,
   location: String,
-  price: Number
+  price: Number,
+  lat: Number,
+  lon: Number
 });
 
 var userSchema = mongoose.Schema({
@@ -46,16 +49,25 @@ app.get('/', function(req, res) {
 })
 app.get('/find', function(req, res) {
   
+  console.log(req.query.location);
+  
   var home = [];
   UserModel.find(function(err, users) {
     for(var i=0; i<users.length; i++) {
       for(var y=0; y<users[i].home.length; y++) {
         //console.log(users[i].home[y]);
-        home.push(users[i].home[y]);
+       
+        var locationForm = req.query.location.trim();
+        var locationBDD  = users[i].home[y].location; 
+        if(locationBDD != undefined && locationBDD.indexOf(locationForm) != -1) {
+        //if(locationBDD == locationForm) {
+          home.push(users[i].home[y]);
+        }
+
       }
     }
     
-    console.log(home);
+    //console.log(home);
     res.render("find", {homeList : home});
   });
   
@@ -123,25 +135,32 @@ app.get('/hote', function(req, res) {
 app.post('/register-hote', function(req, res) {
   
 
-  console.log(req.query.title+'/'+req.query.desc+'/'+req.query.location+'/'+req.query.price);
-
   UserModel.find( { email: req.session.email, password: req.session.password} , function (err, user) {
-
-    user[0].home.push({  
-      title: req.body.title,
-      desc: req.body.desc,
-      location: req.body.location,
-      price: req.body.price
-    });
-
-    user[0].save(function() {
-        var total = user[0].home.length;
-        
-        req.files.picture.mv('./public/img/home/'+user[0].home[total-1].id+'.jpg', function(err) {
-        });
-        res.redirect("/");
-    });
     
+
+    request("http://free.gisgraphy.com/geocoding/geocode?address="+req.body.location+"&country=fr&postal=true&format=JSON&from=1&to=10&indent=false", function(error, response, body) {
+      body = JSON.parse(body);
+      
+      user[0].home.push({  
+        title: req.body.title,
+        desc: req.body.desc,
+        location: req.body.location,
+        price: req.body.price,
+        lat: body.result[0].lat,
+        lon: body.result[0].lng
+      });
+
+      user[0].save(function() {
+          var total = user[0].home.length;
+          
+          req.files.picture.mv('./public/img/home/'+user[0].home[total-1].id+'.jpg', function(err) {
+          });
+          res.redirect("/");
+      });
+    
+    });
+
+   
     
   })
   
